7af55086eae7e931ceb2ea96d812e975db24c7ee
mossxzzom <mossxzzom@gmail.com>
2022-09-24 10:32:25 +0800

    fix: 订阅功能无法使用. #82 (#83)

--- page_end/header.js
+++ page_end/header.js
@@ -1,34 +1,33 @@
 /**
  * 博客标题导航等
- * @namespace header
+ * @namespace headers
+ */
+
+/**
+ * @typedef {Object} header
+ * @property {String} url - 主页url
+ * @property {String} title - https://i.cnblogs.com/settings - 博客标题
+ * @property {String} subtitle - https://i.cnblogs.com/settings - 博客子标题
+ * @property {String} navlist - https://i.cnblogs.com/preference - 导航栏控件
  */
 
 /**
  * header数据加载
- * @memberof header
- * @param {Element} originBody
+ * @memberof headers
+ * @returns {header} 博客标题导航
  */
-function GetHeader(originBody) {
-    // def
+function GetHeader() {
     const header = (vm.header = {})
-    const headerdom = originBody.querySelector('#header')
-    // h1h2
+    const headerdom = vm.metadata.querySelector('#header')
+    // title subtitle
     const h1dom = headerdom.querySelector('h1 a')
     header.url = h1dom.href.trim()
     header.title = h1dom.innerText.trim()
     const h2dom = headerdom.querySelector('h2')
     header.subtitle = h2dom.innerText.trim()
-    // nav
-    const navdom = headerdom.querySelector('#navList')
-    const lidoms = navdom.querySelectorAll('li a')
-    header.navs = []
-    for (let index = 0; index < lidoms.length; index++) {
-        const lidom = lidoms[index]
-        const nav = {}
-        nav.url = lidom.href.trim()
-        nav.desc = lidom.innerText.trim()
-        header.navs.push(nav)
-    }
+    // navlist
+    const navlistdom = headerdom.querySelector('#navList')
+    header.navlist = navlistdom.innerHTML.trim()
     console.log(header)
     return header
 }
